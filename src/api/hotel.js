082969a5555170c972d298df2c045c5c14eedb14
ailgiP <huangliya@25qp.cn>
2019-03-14 17:27:02 +0800

    完成了添加房间类型的交互

--- src/api/hotel.js
+++ src/api/hotel.js
@@ -8,19 +8,20 @@ export default class decive {
 
   // 添加房间类型
   addDevice(data) {
-    return request.put(`${this.cfg.Base_url}/decive/add`, data)
+    return request.put(`${this.cfg.Base_url}/device/add`, data)
   }
+
+  // 获取所有房间类型
+  getDevice(params) {
+    return request.get(`${this.cfg.Base_url}/device/get`, params)
+  }
+
 
 
   // // 登录
   // login(data) {
   //   // `${this.cfg.Base_url}/login` 相当于 this.cfg.Base_url + '/login'
   //   return request.post(`${this.cfg.Base_url}/login`, data)
-  // }
-  //
-  // // 获取所有员工
-  // getStaff(params) {
-  //   return request.get(`${this.cfg.Base_url}/staff/get`, params)
   // }
   //
 
--- src/pages/admin/device/add/index.vue
+++ src/pages/admin/device/add/index.vue
@@ -5,21 +5,45 @@
     <!--表单内容-->
     <div class="form-container">
       <div class="add-form">
-        <el-form :model="ruleForm" :rules="rules" ref="ruleForm" label-width="100px" class="demo-ruleForm" style="width: 400px;">
-          <el-form-item label="登录账号：" prop="loginName">
-            <el-input v-model="ruleForm.loginName" style="width: 100%"></el-input>
+        <el-form :model="ruleForm" :rules="rules" ref="ruleForm" label-width="120px" class="demo-ruleForm" style="width: 400px;">
+          <el-form-item label="房间类型名字：" prop="name">
+            <el-input v-model="ruleForm.name" style="width: 100%"></el-input>
           </el-form-item>
-          <el-form-item label="真实姓名：" prop="userName">
-            <el-input v-model="ruleForm.userName" style="width: 100%"></el-input>
+          <el-form-item label="床型：" prop="bedType">
+            <el-input v-model="ruleForm.bedType" style="width: 100%"></el-input>
           </el-form-item>
-          <el-form-item label="登录密码：" prop="password">
-            <el-input v-model="ruleForm.password" style="width: 100%"></el-input>
+          <el-form-item label="卫浴类型：" prop="bathroom">
+            <el-input v-model="ruleForm.bathroom" style="width: 100%"></el-input>
           </el-form-item>
-          <el-form-item label="手机号码：" prop="phone">
-            <el-input v-model="ruleForm.phone" style="width: 100%"></el-input>
+          <el-form-item label="容纳人数：" prop="contain">
+            <el-input v-model="ruleForm.contain" type="number" style="width: 100%"></el-input>
           </el-form-item>
-          <el-form-item label="电子邮箱：" prop="email">
-            <el-input v-model="ruleForm.email" style="width: 100%"></el-input>
+          <el-form-item label="有无窗户：" prop="casement">
+            <el-select v-model="ruleForm.casement" placeholder="请选择有无窗户" style="width: 100%">
+              <el-option label="有" value="1"></el-option>
+              <el-option label="无" value="0"></el-option>
+            </el-select>
+          </el-form-item>
+          <el-form-item label="有无wifi：" prop="wifi">
+            <el-select v-model="ruleForm.wifi" placeholder="请选择有无wifi" style="width: 100%">
+              <el-option label="有" value="1"></el-option>
+              <el-option label="无" value="0"></el-option>
+            </el-select>
+          </el-form-item>
+          <!--有wifi才显示-->
+          <template v-if="ruleForm.wifi === '1'">
+            <el-form-item label="wifi名：" prop="wifiName">
+              <el-input v-model="ruleForm.wifiName" style="width: 100%"></el-input>
+            </el-form-item>
+            <el-form-item label="wifi密码：" prop="wifiPassword">
+              <el-input v-model="ruleForm.wifiPassword" style="width: 100%"></el-input>
+            </el-form-item>
+          </template>
+          <el-form-item label="房间面积：" prop="size">
+            <el-input v-model="ruleForm.size" type="number" style="width: 100%"></el-input>
+          </el-form-item>
+          <el-form-item label="房间价格：" prop="price">
+            <el-input v-model="ruleForm.price" type="number" style="width: 100%"></el-input>
           </el-form-item>
           <el-form-item>
             <el-button type="primary" @click="submitForm()">立即创建</el-button>
@@ -41,32 +65,42 @@
       return {
         // 表单数据
         ruleForm: {
-          hotelId: 1,
-          role: 'staff',
-          loginName: null,
-          userName: null,
-          password: null,
-          phone: null,
-          email: null
+          name: null,
+          bedType: null,
+          bathroom: null,
+          contain: null,
+          casement: null,
+          wifi: null,
+          wifiName: null,
+          wifiPassword: null,
+          size: null,
+          price: null
         },
         // 表单验证数据
         rules: {
-          loginName: [
-            { required: true, message: '请填写登录账号', trigger: 'blur' }
+          name: [
+            { required: true, message: '请填写房间类型名字', trigger: 'blur' }
           ],
-          userName: [
-            { required: true, message: '请填写真实姓名', trigger: 'blur' }
+          bedType: [
+            { required: true, message: '请填写床型', trigger: 'blur' }
           ],
-          password: [
-            { required: true, message: '请填写登录密码', trigger: 'blur' }
+          bathroom: [
+            { required: true, message: '请填写卫浴类型', trigger: 'blur' }
           ],
-          phone: [
-            { required: true, message: '请输入手机号码', trigger: 'blur' },
-            { pattern: /^1[3456789]\d{9}$/, message: '请输入正确的手机号码' }
+          contain: [
+            { required: true, message: '请输入容纳人数', trigger: 'blur' },
           ],
-          email: [
-            { required: true, message: '请输入邮箱地址', trigger: 'blur' },
-            { pattern: /\w+([-+.]\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*/, message: '请输入正确的邮箱地址' }
+          casement: [
+            { required: true, message: '请选择有无窗户', trigger: 'change' },
+          ],
+          wifi: [
+            { required: true, message: '请选择有无wifi', trigger: 'change' },
+          ],
+          size: [
+            { required: true, message: '请输入房间面积', trigger: 'blur' },
+          ],
+          price: [
+            { required: true, message: '请输入房间价格', trigger: 'blur' },
           ]
         }
       };
@@ -78,14 +112,18 @@
         this.$refs['ruleForm'].validate(async(valid) => {
           // 通过了表单验证
           if (valid) {
-            console.log(this.ruleForm)
+            if (!this.ruleForm.wifi) {
+              this.ruleForm.wifiName = ''
+              this.ruleForm.wifiPassword = ''
+            }
+            // console.log(this.ruleForm)
             // 调取后台接口
             const res = await Hotel_api.addDevice(this.ruleForm)
             if (res.data.code === 0) {
               // 饿了么提示UI
-              this.$message.success('新增员工成功！')
+              this.$message.success('新增房间类型成功！')
               // 路由跳转到宠物管理页面
-              this.$router.push({ path: '/staff' })
+              this.$router.push({ path: '/admin/device' })
             }
             // 错误，显示错误
             else {
